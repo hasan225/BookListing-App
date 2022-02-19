82a124225f613ed9bf0e30787ac45c3d5a73c7bf
bijoy <uzumakibijoy2017@gmail.com>
2022-02-19 12:35:47 +0600

    bookListing App completed

---
+++ app.js
@@ -0,0 +1,50 @@
+const title =document.querySelector("#title");
+const author =document.querySelector("#author");
+const year =document.querySelector("#year");
+const btn =document.querySelector(".btn");
+const bookList =document.querySelector("#book-list");
+const warning =document.querySelector(".warning");
+
+//  if(message.value===""){
+//        feedback.style.display="block";
+//      setTimeout(()=>{
+//          feedback.style.display="none";
+
+//      },3000)
+       
+//    }
+btn.addEventListener("click",(e)=>{
+   e.preventDefault()
+   
+   if(title.value==="" || author.value==="" || year.value===""){
+     warning.textContent=" *PLEASE FILL UP ALL THE INPUTS *"
+     setTimeout(()=>{
+       warning.style.display="none";
+     },2000)
+   }
+
+   else{
+      // message_content.textContent=message.value;
+      //  message.value='';
+    //  bookList.textContent=title.value
+
+        const newRow=document.createElement('tr');
+        const newTitle=document.createElement('td');
+        newTitle.innerHTML=title.value;
+        newRow.appendChild(newTitle);
+
+        const newAuthor=document.createElement('td');
+        newAuthor.innerHTML=author.value;
+        newRow.appendChild(newAuthor);
+
+
+        const newYear=document.createElement('td');
+        newYear.innerHTML=year.value;
+        newRow.appendChild(newYear);
+         
+        bookList.appendChild(newRow)
+
+        // newTd.textContent=title.value
+        // console.log(newTd)
+   }
+})
